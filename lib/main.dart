013a5e0c73658a60bfc2aed5d4e40ab65569662c
Aatmjeet Singh <aatmjeetsingh18@gmail.com>
2021-10-08 10:49:54 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:io';
+import 'package:flutter/foundation.dart' show kIsWeb;
+//import 'package:flutter/widgets.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData.dark(),
+      home: const MyHomePage(title: 'Color picker'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final Map<String, Color> colors = {
+    'purple': Colors.purple,
+    'blue': Colors.blue,
+    'yellow': Colors.yellow,
+    'pink': Colors.pink,
+    'teal': Colors.teal,
+    'orange': Colors.orange
+  };
+
+  Color? selectedColor;
+
+  @override
+  void initState() {
+    _getStoredColor();
+    super.initState();
+  }
+
+  void _getStoredColor() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String? colorName = prefs.getString('color');
+    setState(() {
+      selectedColor = colors[colorName];
+    });
+  }
+
+  void _setColor(String colorName, Color color) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setString('color', colorName);
+    setState(() {
+      selectedColor = color;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+        backgroundColor: selectedColor ?? Colors.black,
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: Text(
+              'You are operating on ${kIsWeb ? "the Web" : Platform.operatingSystem}',
+              style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+          ),
+          for (var entry in colors.entries)
+            Container(
+              margin: const EdgeInsets.all(10),
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  primary: entry.value,
+                  minimumSize: const Size(300, 60),
+                ),
+                child: const Text(''),
+                onPressed: () => _setColor(entry.key, entry.value),
+              ),
+            )
+        ],
+      ),
+    );
+  }
+}
